79a48e487bf6e3d1b5acb1d6011302d63b26e496
Artem Bilodid <bilodid.sumy@gmail.com>
2021-07-23 10:40:59 +0300

    task 1 finished

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categories = document.querySelector("ul#categories");
+
+let logCategoryInfo = (category) => {
+  const categoryName = category.querySelector("li.item>h2").innerText;
+  const elementsLength = category.querySelectorAll("li").length;
+  console.log(`Категория: ${categoryName}`);
+  console.log(`Количество элементов: ${elementsLength}`);
+};
+
+[...categories.children].forEach(logCategoryInfo);
